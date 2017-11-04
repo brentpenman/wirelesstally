577116949b8e67b822c3e643d4f6ceca352f3b54
Brent Penman <brentpenman@gmail.com>
2017-11-04 08:33:26 -0400

    Added Comments Set more Static Variables

--- client.py
+++ client.py
@@ -1,3 +1,9 @@
+##TODO - What a mess
+##TODO - Read and assign all pins
+##TODO - Set pins high or low depending on state of some setup pin
+##TODO - Verify proper implementation of sockets
+##TODO - Needs proper logging
+
 #! /usr/bin/env python
 import sys
 import time
@@ -6,42 +12,43 @@ import RPi.GPIO as GPIO
 
 TALLYPIN=4
 STATUSPIN=20
+SERVERADDRESS="10.2.1.1"
+SERVERPORT=10000
 
+#Initialize GPIO pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(TALLYPIN, GPIO.OUT)
 GPIO.setup(STATUSPIN, GPIO.OUT)
 
+#Continuously attempt to connect
 while True:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ("10.2.1.1", 10000)
+    server_address = (SERVERADDRESS, SERVERPORT)
     sock.settimeout(1)
     try:
-        #print "connecting"
         sock.connect(server_address)
+	#Set Status Pin to indicate connection
         GPIO.output(STATUSPIN, 0)
-        #print "connected"
+	#When connected, continuously attempt to get state
         while True:
             try:
-                #print "sending"
                 sock.sendall("?")
-               #print "receiving"
                 data = sock.recv(16)
                 if not data:
-	           #print "nodataCLOSE"
-                    sock.close()
-                    #time.sleep(1)
+	            sock.close()
                     break
-		
+		#Reverse incoming State so High Becomes low
+		#TODO - Make more versatile for different types of systems
                 datanum = 1 - int(data.strip())
                 print datanum
-                GPIO.output(TALLYPIN, int(data.strip()))
-                #time.sleep(0.2)
+                GPIO.output(TALLYPIN, int(data.strip()))                
             except Exception as e:
                 print "SENDRECVEXCEPTION: " + str(e)
                 sock.close()
                 time.sleep(1)
                 break
     except Exception as e:
+        #Wait 1 second before attempting reconnection, flash status pin during
         print "Can't connect: " + str(e)
 	GPIO.output(STATUSPIN, 1)
 	time.sleep(0.5)
--- server.py
+++ server.py
@@ -1,33 +1,50 @@
+##TODO Needs Proper Logging
+##TODO Get and send state of ALL pins
+##TODO Verify proper implementation of sockets, especially around closing
+##TODO Check one pin to expect low or high for trigger
+
 import socket
 import sys
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+#Define Ports
+TRIGGERPIN=4
+BINDADDRESS='0.0.0.0'
+BINDPORT=10000
 
+#Initialize GPIO Pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIGGERPIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+#Setup Server Socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ( '0.0.0.0', 10000)
+server_address = ( BINDADDRESS, BINDPORT)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(server_address)
-#sock.settimeout(2)
 sock.listen(20)
 
+#Primary Loop
 while True:
-    connection, client_address = sock.accept()          
-    print client_address
+    #Always Accept New Connections
+    connection, client_address = sock.accept()    
+    print client_address          
+    #When Connected, Continuously expect requests
     while True:
         try:
             data = connection.recv(16)
 	    print data
+            #Close connection if no data
             if not data:
 		print "no data"
                 connection.close()                
-            state = str(GPIO.input(4))
+            #Send current Trigger Pin State
+            state = str(GPIO.input(TRIGGERPIN))
             connection.send(state)        
-            
-            
+        #Close on problems                
         except:
-            print "closing"           
+            print "closing"
+	    ##TODO I had this connection.close() line removed for some reason?
+	    connection.close()
             break
 
